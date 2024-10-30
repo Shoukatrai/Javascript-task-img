2441f05a2c74e6a48946b34434c9a13e95225c61
Shoukatrai <raishoukat033@gmail.com>
2024-10-31 01:00:11 +0500

    Event Assignment

---
+++ app.js
@@ -0,0 +1,50 @@
+//Image 1
+function lambCar(lambCar){
+    lambCar.src = "./Images/lambCar.gif";
+}
+
+function lambCarOut(lambCar2){
+    lambCar2.src = "./Images/lambCar.jpg";
+}
+
+
+//Image 2
+function spiderman(spiderman){
+    spiderman.src = "./Images/spiderman1.gif";
+}
+
+function spidermanOut(spidermanOut){
+    spidermanOut.src = "./Images/spiderman2.jpeg";
+}
+
+//Image 3
+function spiderman2(spiderman2){
+    spiderman2.src = "./Images/spiderman2.gif";
+}
+
+function spiderman2Out(spiderman2out){
+    spiderman2out.src = "./Images/spiderman2.jpg";
+}
+
+//Image 4
+
+function bmwCar(bmwCar){
+    bmwCar.src = "./Images/BMW drift.gif"
+}
+
+
+function bmwCarOut(bmwCar2){
+    bmwCar2.src = "./Images/bmw m4 comp.jpeg"
+}
+
+
+
+//Image 5
+function flag(flag){
+    flag.src = "./Images/flag.gif"
+}
+
+
+function flagOut(flag){
+    flag.src = "./Images/flag.jpeg"
+}
